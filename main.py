bd282116404268c2d0dfdbf016f8c7aa81292781
Nikunj Jain <nik@Nikunjs-MacBook-Air.local>
2024-01-17 21:36:58 +0530

    changes

---
+++ main.py
@@ -0,0 +1,33 @@
+import threading
+import time
+from functools import wraps
+def rate_limiter(max_calls, period):
+  def decorator(func):
+    last_rest = time.time()
+    count = 0
+    lock = threading.Lock()
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+      nonlocal last_rest, count
+      curr = time.time()
+      with lock:
+        if curr-last_rest>period:
+          count = 0
+          last_rest = curr
+        if max_calls>count:
+          count+=1
+          return func(*args,**kwargs)
+        else:
+          raise ValueError("Terminated Limit reached")
+    return wrapper
+  return decorator
+
+@rate_limiter(max_calls = 3, period = 15)
+def func(x,y):
+  print(x+y)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+  for _ in range(34):
+    func(3,5)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
